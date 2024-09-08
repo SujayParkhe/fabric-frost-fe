e28cdb18885208bffb856a2583081800c28b9b40
sujay parkhe <sparkhe852@gmail.com>
2024-09-08 14:52:57 +0530

    setting up tailwind vite typescript and shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello Fabric Frost</h1>
+      <Button>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
